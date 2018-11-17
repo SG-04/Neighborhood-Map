4302f1d0e8aea37f320602074f8ad2d55dd625a2
Shruti Gupta <shrutigupta.sg33@gmail.com>
2018-11-18 00:39:50 +0530

    add few additional checks in react's service worker registration.

--- src/App.js
+++ src/App.js
@@ -1,93 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
+import { Header, Search } from './components';
 
-class App extends Component {
-
+class NeighborhoodApp extends Component {
+// App's internal state
   state = {
-    venues: []
+    isListOpen: false,
   }
 
-  componentDidMount(){
-    this.getVenues()
-  }
-
-  loadMap = () => {
-    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyD9x0NjNq08cqACzrapAeq8NCjRhFRrcD4&callback=initMap")
-    window.initMap=this.initMap
-  }
-
-  getVenues = () => {
-    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
-    const parameters = {
-      client_id: "MFWBYGLPDEHOGIVGLHV5NJ15OKXXHGKDQU0VJEROPTKWGVSO",
-      client_secret: "KUJCYLVMHE4XX0YINYCEJ0ITW4EQBEHYWLN3C5RDBR5QW43S",
-      query:"food",
-      near: "Sydney",
-      v:"20181117"
-    }
-
-    axios.get(endPoint + new URLSearchParams(parameters))
-    .then(response => {
-      this.setState({venues:response.data.response.groups[0].items},
-      this.loadMap())
-    })
-    .catch(error => {
-      console.log("ERROR!!" + error)
-    })
-
-  }
-  
-  initMap = () => {
-
-    //Create a map
-    var map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
-
-    //Create an Infowindow
-    var infowindow = new window.google.maps.InfoWindow();
-
-    //Display dynamic markers
-    this.state.venues.map(myVenue => {
-
-      var contentString = `${ myVenue.venue.name}`;
-
-      //Create a Marker
-      var marker = new window.google.maps.Marker({
-        position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
-        map: map,
-        title: myVenue.venue.name
-      })
-
-      //Click on a marker
-      marker.addListener('click', function() {
-
-        //Change the content
-        infowindow.setContent(contentString);
-        
-        //Open an Infowindow
-        infowindow.open(map, marker);
-      });
-    });
+  /**
+   * Even handler for Toggle component,it sets the app state to
+   * indicate if list view is open or closed based on hamburger menu icon
+   * @param:
+   *      None
+   * @returns:
+   *      None
+   */
+  showListView = () => {
+    this.setState(prevState => ({
+      isListOpen: !prevState.isListOpen,
+    }));
   }
 
   render() {
+    const { isListOpen } = this.state;
     return (
-      <main>
-        <div id="map"></div>
-      </main>
+      <div className="App">
+        <Header showPlaceList={this.showListView} />
+        <Search isListOpen={isListOpen} />
+      </div>
     );
   }
 }
 
-function loadScript(url){
-  var index=window.document.getElementsByTagName("script")[0]
-  var script=window.document.createElement("script")
-  script.src=url
-  script.async=true
-  script.defer=true
-  index.parentNode.insertBefore(script,index)
-}
-export default App;
+export default NeighborhoodApp;
