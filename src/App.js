06fc9858aa1ea0822063c0c1ab995627adf1fd5b
Shruti Gupta <shrutigupta.sg33@gmail.com>
2018-11-18 00:53:31 +0530

    fix some issues

--- src/App.js
+++ src/App.js
@@ -15,16 +15,7 @@ class App extends Component {
    *      None
    * @returns:
    *      None
-   */
-  componentDidMount() {
-    fetch(`https://api.foursquare.com/v2/venues/search?ll=26.14,78.10&client_id=MFWBYGLPDEHOGIVGLHV5NJ15OKXXHGKDQU0VJEROPTKWGVSO&client_secret=KUJCYLVMHE4XX0YINYCEJ0ITW4EQBEHYWLN3C5RDBR5QW43S&limit=25&v=20181117`)
-    .then( response => response.json())
-    .then( data => {
-      let locations = data.response.venues;
-      this.setState({ locations });
-    })
-    .catch(error => console.log(error));
-  }      	
+   */    	
   
   showListView = () => {
     this.setState(prevState => ({
