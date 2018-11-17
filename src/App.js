481db054d7f3bc45df1ba606ee540c288b36d599
Shruti Gupta <shrutigupta.sg33@gmail.com>
2018-11-18 00:44:29 +0530

    modified css to make app responsive

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Header, Search } from './components';
 
-class NeighborhoodApp extends Component {
+class App extends Component {
 // App's internal state
   state = {
     isListOpen: false,
@@ -16,6 +16,16 @@ class NeighborhoodApp extends Component {
    * @returns:
    *      None
    */
+  componentDidMount() {
+    fetch(`https://api.foursquare.com/v2/venues/search?ll=26.14,78.10&client_id=MFWBYGLPDEHOGIVGLHV5NJ15OKXXHGKDQU0VJEROPTKWGVSO&client_secret=KUJCYLVMHE4XX0YINYCEJ0ITW4EQBEHYWLN3C5RDBR5QW43S&limit=25&v=20181117`)
+    .then( response => response.json())
+    .then( data => {
+      let locations = data.response.venues;
+      this.setState({ locations });
+    })
+    .catch(error => console.log(error));
+  }      	
+  
   showListView = () => {
     this.setState(prevState => ({
       isListOpen: !prevState.isListOpen,
@@ -33,4 +43,4 @@ class NeighborhoodApp extends Component {
   }
 }
 
-export default NeighborhoodApp;
+export default App;
