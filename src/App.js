5d770e0e36ba00469385e75238344bf653ad2095
Shruti Gupta <shrutigupta.sg33@gmail.com>
2018-11-17 20:10:24 +0530

    adding Foursquare api

--- src/App.js
+++ src/App.js
@@ -4,15 +4,39 @@ import axios from 'axios';
 
 class App extends Component {
 
+  state = {
+    venues: []
+  }
+
   componentDidMount(){
+    this.getVenues()
     this.loadMap()
   }
 
   loadMap = () => {
-    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBi9jzy6u1WBYJj9AIBKHu2MOPPg6oYpA0&callback=initMap")
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyD6VCMBe5VKBrf0lING4q0wpmrwc3UYuwU&callback=initMap")
     window.initMap=this.initMap
   }
 
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore"
+    const parameters = {
+      client_id: "MFWBYGLPDEHOGIVGLHV5NJ15OKXXHGKDQU0VJEROPTKWGVSO",
+      client_secret: "KUJCYLVMHE4XX0YINYCEJ0ITW4EQBEHYWLN3C5RDBR5QW43S",
+      query:"food",
+      near: "Sydney",
+      v:"20181117"
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+    .then(response => {
+      this.setState({venues:response.data.response.groups[0].items})
+    })
+    .catch(error => {
+      console.log("ERROR!!" + error)
+    })
+
+  }
   
   initMap = () => {
     var map = new window.google.maps.Map(document.getElementById('map'), {
