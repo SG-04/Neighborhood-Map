8a3cf8530c178aad5e91ef1c234939180e562f5f
Shruti Gupta <shrutigupta.sg33@gmail.com>
2018-11-17 23:01:50 +0530

    add few additional checks in react's service worker registration.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -133,3 +133,37 @@ export function unregister() {
     });
   }
 }
+
+/**
+ * listen for the installing service worker state
+ * and if installed we inform the user about update
+ */
+function trackInstalling(installingWorker) {
+  installingWorker.addEventListener('statechange', () => {
+    // newWorker.state has changed
+    if (installingWorker.state === 'installed') {
+      if (navigator.serviceWorker.controller) {
+        // At this point, the old content will have been purged and
+        // the fresh content will have been added to the cache.
+        // It's the perfect time to display a "New content is
+        // available; please refresh." message in your web app.
+        console.log('New content is available; please refresh.');
+        updateReady(installingWorker);
+      } else {
+        // At this point, everything has been precached.
+        // It's the perfect time to display a
+        // "Content is cached for offline use." message.
+        console.log('Content is cached for offline use.');
+      }
+    }
+  });
+}
+
+function updateReady(worker) {
+  let userConsent = false;
+  userConsent = window.confirm('New version available. Do you want to update?');
+  if (!userConsent) return;
+  // tell the service worker to skipWaiting
+  // console.log('updateSW');
+  worker.postMessage('updateSW');
+}
