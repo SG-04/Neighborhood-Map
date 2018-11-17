252447ea21a67895a760498ddd0d32143d99b052
Shruti Gupta <shrutigupta.sg33@gmail.com>
2018-11-18 00:44:50 +0530

    add few additional checks in react's service worker registration.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -60,6 +60,7 @@ function registerValidSW(swUrl, config) {
     .then(registration => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
+        trackInstalling(installingWorker);
         if (installingWorker == null) {
           return;
         }
