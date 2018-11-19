aded3d9b4dba9479b9a044e5acf6a4911cadffc5
Shruti Gupta <shrutigupta.sg33@gmail.com>
2018-11-19 14:54:03 +0530

    Updates

---
+++ precache-manifest.5221c846a7adadbeaec3019c1bbb3f4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "88236936de1f1fe23595",
+    "url": "/static/js/main.88236936.chunk.js"
+  },
+  {
+    "revision": "bf085f87323a80e9c1a3",
+    "url": "/static/js/1.bf085f87.chunk.js"
+  },
+  {
+    "revision": "88236936de1f1fe23595",
+    "url": "/static/css/main.92beeb17.chunk.css"
+  },
+  {
+    "revision": "bae328f6ac9d1b5c5f390f1b5887e106",
+    "url": "/index.html"
+  }
+];
